96d852b807b9dc4281c1ebf55b58b0577fbf789e
Misha <konokradkonokrad@gmail.com>
2021-03-16 10:40:04 +0200

    Multiply is done

--- номінали/Nominal.cs
+++ номінали/Nominal.cs
@@ -37,7 +37,6 @@ namespace номінали
         public static bool operator !=(Nominal obj1, Nominal obj2)
         {
             return (obj1.GetSum() != obj2.GetSum());
-
         }
 
         public static SumOfMoney operator +(Nominal obj1, Nominal obj2)
@@ -72,8 +71,9 @@ namespace номінали
 
         public static double operator *(Nominal obj1, Nominal obj2)
         {
-           double sumOfMoney = obj1.sumOfMoney.grn + obj2.sumOfMoney.grn*100 + obj1.sumOfMoney.sumOfCoins + obj2.sumOfMoney.sumOfCoins;
-            return sumOfMoney;
+           double sumOfMoney = obj1.sumOfMoney.grn*100 + obj1.sumOfMoney.sumOfCoins;
+           double sumOfMoney2 = obj2.sumOfMoney.grn*100 + obj2.sumOfMoney.sumOfCoins;
+            return sumOfMoney*sumOfMoney2;
         }
 
         public static double operator /(Nominal obj1, Nominal obj2)
